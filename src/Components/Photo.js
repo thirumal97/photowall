6cea4bbf95b5657cfc7758598b4c8c0581e98220
thirumal97 <thirumalkonatham1@gmail.com>
2021-04-03 16:00:18 -0400

    WIP: stateless functions

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 //PhotoWall component instanace passess ana array of posts and map the array to render out three phot cpmpnent instances ,
 //And for every photo instance that is created for each post that we're iterating through we're passing
 //in props of the post associate it to that photo that was just looped through.
 
-
-class Photo extends Component {
-    render() {
-        const post = this.props.post
+function Photo(props) {
+    const post = props.post
         return <figure className="figure">
                     <img className = "photo" src = {post.imageLink} alt={post.description} />
                     <figcaption> <p> {post.description} </p></figcaption>
@@ -15,7 +13,7 @@ class Photo extends Component {
                         <button className="remove-button"> Remove </button>
                     </div>
                 </figure>
-    }
 }
+
 
 export default Photo
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,10 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Photo from './Photo'
 
-class PhotoWall extends Component {
-    render() {
-        return  <div className="photoGrid">
-            {this.props.posts.map((post, index) => <Photo key = {index} post = {post} /> )}
+function PhotoWall(props) {
+    return <div className="photoGrid">
+            {props.posts.map((post, index) => <Photo key = {index} post = {post} /> )}
         </div>
-    }
 }
 
 
